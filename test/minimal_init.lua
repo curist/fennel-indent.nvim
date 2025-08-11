f6ab8079bc0fa48f47e7ad1eb8a5e4c6c3be4a88
curist <curist.cyc@gmail.com>
2025-08-11 15:20:55 +0800

    let's try with ftplugin on

--- test/minimal_init.lua
+++ test/minimal_init.lua
@@ -1,3 +1,5 @@
 -- Minimal init.lua for headless nvim integration testing
 local plugin_path = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h:h")
 vim.opt.runtimepath:prepend(plugin_path)
+
+vim.cmd "filetype plugin indent on"
