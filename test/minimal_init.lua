ecec558864f9876f14a1fdf928f94f78ff3400ed
curist <curist.cyc@gmail.com>
2025-08-11 15:29:11 +0800

    let's use neovim from ubuntu repo for CI

--- test/minimal_init.lua
+++ test/minimal_init.lua
@@ -1,5 +1,3 @@
 -- Minimal init.lua for headless nvim integration testing
 local plugin_path = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h:h")
 vim.opt.runtimepath:prepend(plugin_path)
-
-vim.cmd "filetype plugin indent on"
