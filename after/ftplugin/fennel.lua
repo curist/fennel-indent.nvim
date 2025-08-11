12c8b59b17d8f74dea88a3af0132bebbe8a35c3e
curist <curist.cyc@gmail.com>
2025-08-11 14:57:58 +0800

    reafactor: simplify ftplugin setup and add iskeyword symbols
    also simplify test/minimal_init.lua

--- after/ftplugin/fennel.lua
+++ after/ftplugin/fennel.lua
@@ -2,11 +2,15 @@
 vim.bo.lisp = false
 vim.bo.smartindent = false
 vim.bo.cindent = false
-vim.bo.autoindent = true  -- Keep for basic functionality
+vim.bo.autoindent = true
 
--- Set up our custom indenter  
-vim.bo.indentexpr = 'v:lua.require("fennel-indent").indentexpr()'
-vim.bo.indentkeys = '0{,0},0),0],!^F,o,O,e,;'
-
--- Set up our custom formatter for gq/gg=G commands
-vim.bo.formatexpr = 'v:lua.require("fennel-indent").formatexpr()'
+-- since we are setting nolisp, let's add some keyword chars back
+local symbols = { "-", "?", "!" }
+local ik = vim.bo.iskeyword
+for _, sym in ipairs(symbols) do
+  -- match only if symbol exists as a whole item
+  if not ik:match("(^|,)" .. vim.pesc(sym) .. "($|,)") then
+    ik = ik .. "," .. sym
+  end
+end
+vim.bo.iskeyword = ik
